6baea5813f73434f13c55f9b65f5cde71bf77865
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-06-27 00:49:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 que = {}
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
 API_ID = int(getenv("API_ID", "6435225"))
 API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
