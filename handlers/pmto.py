3a359658717c371185f8ccd13b127c1471462d5f
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-06-27 01:13:34 +0530

    Create pmto.py

---
+++ handlers/pmto.py
@@ -0,0 +1,20 @@
+@amaan.on_message(filters.command("pmto", HANDLER) & filters.me)
+@amaan.on_message(filters.command("pmto", HANDLER) & filters.user(SUDOERS))
+async def pmto(client, message):
+    a = message.pattern_match.group(1)
+    b = a.split(" ")
+    chat_id = b[0]
+    try:
+        chat_id = int(chat_id)
+    except BaseException:
+        pass
+    msg = ""
+    for i in b[1:]:
+        msg += i + " "
+    if msg == "":
+        return
+    try:
+        await client.send_message(chat_id, msg)
+        await message.edit_text("Message sent!")
+    except BaseException:
+        await message.edit_text("Something went wrong.")
