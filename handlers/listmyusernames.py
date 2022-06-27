ee0e3d189e205d2cf2f45c8011d5f8c795b7f2dc
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-06-27 08:14:29 +0530

    Update listmyusernames.py

--- handlers/listmyusernames.py
+++ handlers/listmyusernames.py
@@ -8,7 +8,7 @@ from main import SUDOERS
 @Client.on_message(filters.command(["listmyusernames", "lmun"], [".", "!"]) & filters.me)
 @Client.on_message(filters.command(["listmyusernames", "lmun"], [".", "!"]) & filters.user(SUDOERS))
 async def listmun(c: Client, m: Message):
-    result = await Client.send(GetAdminedPublicChannels())
+    result = await Client.send_data(GetAdminedPublicChannels())
     output_str = ""
     for channel_obj in result.chats:
         output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
