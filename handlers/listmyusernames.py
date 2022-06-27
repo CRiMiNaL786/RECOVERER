53472db6380bb5403a008e1fdf692969921e728d
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-06-27 08:09:18 +0530

    Update listmyusernames.py

--- handlers/listmyusernames.py
+++ handlers/listmyusernames.py
@@ -3,9 +3,10 @@ from pyrogram.raw.functions.channels import GetAdminedPublicChannels
 from pyrogram import Client , filters
 import asyncio
 from pyrogram.types import Message
+from main import SUDOERS
 
-@Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.me)
-@Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.user(SUDOERS))
+@Client.on_message(filters.command(["listmyusernames", "lmun"], [".", "!"]) & filters.me)
+@Client.on_message(filters.command(["listmyusernames", "lmun"], [".", "!"]) & filters.user(SUDOERS))
 async def listmun(c: Client, m: Message):
     result = await Client.send(GetAdminedPublicChannels())
     output_str = ""
