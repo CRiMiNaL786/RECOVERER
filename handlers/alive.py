67b251a2c5e7b9dc15063e5606aa6609cb9a5982
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-07-04 08:29:26 +0530

    Update and rename Ping.py to alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -6,6 +6,14 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
 
+    await message.delete()
+    text="**RECOVERER**\n"
+    text += f"\nPython Version: `{version_info[0]}.{version_info[1]}.{version_info[2]}`"
+    text += f"\nPyrogram Version: `{__version__}`"
+    text += f"\nCurrent Uptime: `{str(datetime.now() - StartTime).split('.')[0]}`"
+    
+    await c.send_message(chat.id, text)
+
 
 @Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.me)
 @Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.user(SUDOERS))
